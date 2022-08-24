aaebb59818f2b87ed5bf66606e139401f818d61a
何方 <he529564582@163.com>
2022-08-24 17:13:23 +0800

    add components

---
+++ src/layout/footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="footer">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GuluFooter'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+      height: 20%;
+      background:#a1eafb;
+  }
+</style>
---
+++ src/layout/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GuluHeader'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .header {
+      height: 20%;
+      background:#00adb5;
+  }
+</style>
